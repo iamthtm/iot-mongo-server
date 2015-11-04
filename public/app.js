b0c3b48d1856c3473a5e57069b95b6382eebc746
iamthtm <p_1624_n@hotmail.com>
2015-11-04 07:23:18 +0700

    แก้server.js

--- public/app.js
+++ public/app.js
@@ -3,7 +3,6 @@ angular.module('todoApp', [])
     var todoList = this;
     
     getiot()
-    
     var count = 0
     $interval(function () 
     {
--- server.js
+++ server.js
@@ -2,38 +2,17 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var Model = require('./models/iot.js')
-var iot = require('./models/iot.js')
 mongoose.connect('mongodb://localhost/iot')
 
 var app = express()
 
 app.use(express.static('public'))
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use('/api', require('./route/api'))
 
-app.post('/', function (req, res) {
-  var obj = new Model(req.body)
-  obj.save(function (err, obj) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.send(obj)
-    }
-  })
-})
-
-app.get('/', function (req, res) {
-  Model.find({}).exec(function (err, results) {
-    if (err) {
-      res.status(500).send(err)
-    } else {
-      res.send(results)
-    }
-  })
-})
+app.use('/', require('./route/api.js'))
 
 
 app.listen(3000)
