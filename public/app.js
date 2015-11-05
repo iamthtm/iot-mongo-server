97aa33667754d169c835ccc9c7dee1643c830307
iamthtm <p_1624_n@hotmail.com>
2015-11-05 15:33:09 +0700

    edit code

--- public/app.js
+++ public/app.js
@@ -3,9 +3,7 @@ angular.module('todoApp', [])
     var app = this;
       
     app.name = "PING"
-
       getIot()
-
 
       app.toThaiDateTime = function (date) {
       return moment(date).format('MMMM Do YYYY, h:mm:ss a')
@@ -16,11 +14,6 @@ angular.module('todoApp', [])
           //console.log(input)
         }
 
-
-
-
-
-
          function getIot(){
          $http.get('/api/iot'). success(function(response) {
             app.dataiot = response
@@ -30,7 +23,6 @@ angular.module('todoApp', [])
            
             console.log('error')
           });
-
         }
 
        function saveIot(data) {
@@ -46,58 +38,47 @@ angular.module('todoApp', [])
 
 
 
-       app.graph = function(){        
-      console.log("graph working") 
-                $http.get('/api/iot')
-                      .then(function success (response) {
-                        console.log(sessionStorage.testSes)
-                 
-                          var data = {
-                                      labels: [],
-                                      datasets: [
-                                          {
-                                              label: "temperature",
-                                              fillColor: "rgba(255,0,0,0.1)",
-                                              strokeColor: "rgba(255,0,0,1)",
-                                              pointColor: "rgba(255,0,0,1)",
-                                              pointStrokeColor: "#fff",
-                                              pointHighlightFill: "#fff",
-                                              pointHighlightStroke: "rgba(220,220,220,1)",
-                                              data: []
-                                          },
-                                          {
-                                              label: "relative_humidity",
-                                              fillColor: "rgba(69,187,91,0.1)",
-                                              strokeColor: "rgba(69,187,91,1)",
-                                              pointColor: "rgba(69,187,91,1)",
-                                              pointStrokeColor: "#fff",
-                                              pointHighlightFill: "#fff",
-                                              pointHighlightStroke: "rgba(151,187,205,1)",
-                                              data: []
-                                          }
-                                      ]
-                                  };
-
-                       var ctx = document.getElementById("iot").getContext("2d")
-                       var myLineChart = new Chart(ctx).Line(data);
-
-                       
-                          for(var i =0;i<response.data.length;i++){
-                            if (response.data[i].iot_id==1){
-                                 myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,response.data[i].timestamp);
-                               }
-                           
-                        }
-                       
-
-                      }, function error (response) {
-                        alert(response.data.message)
-                      }) 
-              
-             
-            }
-
-
+app.graph = function(){        
+  onsole.log("graph working") 
+    $http.get('/api/iot')
+      .then(function success (response) {
+        console.log(sessionStorage.testSes)
+          var data = {
+              labels: [],
+              datasets: [
+                 {
+                    label: "temperature",
+                    fillColor: "rgba(255,0,0,0.1)",
+                    strokeColor: "rgba(255,0,0,1)",
+                    pointColor: "rgba(255,0,0,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: []
+                  },
+                  {
+                    label: "relative_humidity",
+                    fillColor: "rgba(69,187,91,0.1)",
+                    strokeColor: "rgba(69,187,91,1)",
+                    pointColor: "rgba(69,187,91,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: []
+                  }
+              ]
+          };
+  var ctx = document.getElementById("iot").getContext("2d")
+  var myLineChart = new Chart(ctx).Line(data);
+      for(var i =0;i<response.data.length;i++){
+          if (response.data[i].iot_id==1){
+              myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,response.data[i].timestamp);
+          }
+      }
+      }, function error (response) {
+            alert(response.data.message)
+          }) 
+}
 
 //ลบ
 
--- server.js
+++ server.js
@@ -2,30 +2,27 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var Model = require('./models/iot.js')
+
 mongoose.connect('mongodb://localhost/iot')
 
 var app = express()
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-
-
-app.use('/', require('./route/api.js'))
-
-app.delete('/api/iot/:id', function (req, res) {   
-	Model.findById(req.params.id, function (err, Model) {     
-		Model.remove(function (err) {       
-			if (!err) {         
-				console.log('removed')         
-				return res.send('')       
-			} else {         
-				console.log(err)       
-			}     
-		})   
-	}) 
-})
-
+	app.use(express.static('public'))
+	app.use(bodyParser.urlencoded({extended: true}))
+	app.use(bodyParser.json())
+	app.use('/', require('./route/api.js'))
+//Del
+	app.delete('/api/iot/:id', function (req, res) {
+		Model.findById(req.params.id, function (err, Model) {
+			Model.remove(function (err) {
+				if (!err) {
+					console.log('removed')
+					return res.send('')
+				} else {  
+					console.log(err) 
+				}
+			})
+		})
+	})
 app.listen(3000)
 console.log('run in 3000')
